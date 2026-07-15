abf30c84da29f095b4c7def4344fbce2c188831c
fistonuzabumwana <youfistalline11@gmail.com>
2026-07-15 19:38:10 +0200

    chore: migrate CI workflow to Flutter and update widget tests with BibleBook unit tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,15 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:gospel_hub/main.dart';
+import 'package:gospel_hub/models/bible_book.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const GospelHubApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  test('BibleBook getDisplayName translation mode test', () {
+    final book = BibleBook.allBooks[0]; // Genesis
+    
+    // In English translation mode, it should return English name
+    expect(book.getDisplayName('english'), 'Genesis');
+    
+    // In Parallel or Kinyarwanda translation mode, it should return Kinyarwanda name
+    expect(book.getDisplayName('parallel'), 'Intangiriro');
+    expect(book.getDisplayName('kinyarwanda'), 'Intangiriro');
   });
 }
